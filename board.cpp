e19b8eb8a95c8fe5ab074227294e03c7b99d0c14
YSanchezAraujo <yoelsanchez006@gmail.com>
2020-03-17 22:55:12 -0400

    unicorns fly when we want them to

--- board.cpp
+++ board.cpp
@@ -52,18 +52,53 @@ void init_board() {
     std::cout << bstate1() << std::endl;
 }
 
+void print_array(int arr[5], int pn) {
+    for (int c = 0; c < pn; ++c) {
+        std::cout << arr[c] << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     // spit out initial board to players
     init_board();
-    int playGame = 1;
-    int playerInput;
+    int playGame = 1; // play state
+    int playerInput = 0; // player moves variable
+    int player = 0; // tracker for whose turn it is
+
+    // create move history here
+    int player1Moves[5] = {0, 0, 0, 0, 0};
+    int player2Moves[5] = {0, 0, 0, 0, 0};
+    int player1N = 0;
+    int player2N = 0;
+
+    // how to quit
+    std::cout << "to exit the program enter the number 18303" << std::endl;
     // initial mechanism for keeping the game going
     do {
-	std::cout << "Do you want to play this game?";
-        std::cin >> playerInput;
-        if (playerInput == 0) {
+	// so the way the moves are being stored here is off
+	// ok need to go do something actually productive, tbd
+        if (player % 2 == 0 ? true : false) {
+	    std::cout << "Player 1 enter your move: " << std::endl;
+            player1Moves[player1N] = playerInput;
+	    ++player1N;
+	    print_array(player1Moves, player1N);
+	} else {
+	    std::cout << "Player 2 enter your move: " << std::endl;
+	    player2Moves[player2N] = playerInput;
+	    ++player2N;
+	    print_array(player2Moves, player2N);
+	}
+	++player;
+	// input from user(s)
+	std::cin >> playerInput;
+	// check if game should end
+        if (playerInput == 18303 || player2N > 4 || player1N > 4) {
 	    playGame = 0;
 	}
+	// print out the new state of the game
+	// how do I update the board state?
     } while (playGame == 1);
+    // that's all folks
     return 0;
 }
